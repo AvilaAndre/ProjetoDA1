12faeecd224a7371fc3f1d44e075d25fa51ffcf2
André Ávila <avila_andre14@hotmail.com>
2022-04-13 19:34:05 +0100

    Helper class created

--- Classes/Helper.cpp
+++ Classes/Helper.cpp
@@ -4,76 +4,66 @@
 
 #include "Helper.h"
 
-
-/*
-vector<GameObject> Game::readMap(std::string map_name) {
-    string myText;
-    cout << endl << "Reading map from " << map_name << endl;
-    vector<GameObject> gameObjects;
-    int n_lines = 0, n_columns = 0;
-    // Reads the map from the text file according to its id.
-    ifstream MyReadFile(map_name);
+void Helper::ReadDeliveries(std::vector<Delivery> *deliveries) {
+    std::ifstream MyReadFile("../Input/encomendas.txt");
     int first_line = 0;
     if (MyReadFile.is_open()) {
-        int dimension = 0;
-        string line;
+        std::string line;
         while (getline(MyReadFile, line)) {
             if (first_line == 0) {
-                // Se for a primeira linha do file vou ler as dimens�es e marcar que a primeira linha j� foi lida.
-                for (int letter = 0; letter < line.length(); letter++) {
-                    if (!dimension) {
-                        if (line[letter] != 'x' && line[letter] != ' ') {
-                            n_lines = n_lines * 10;
-                            n_lines += (static_cast<int>(line[letter]) - 48);
-                        }
-                        else if (line[letter] != ' ')
-                        {
-                            dimension++;
-                        }
-                    }
-                    else {
-                        if (line[letter] != ' ') {
-                            n_columns = n_columns * 10;
-                            n_columns += (static_cast<int>(line[letter]) - 48);
-                        }
-                    }
-                }
                 first_line++;
-                dimension = 0;
             }
             else {
-                if (line.length() < n_columns) {
-                    cout << "ERROR: Map file's columns number does not correspond." << endl;
-                    return {};
-                }
-                //cout << line <<  endl;
-                for (int letter = 0; letter < n_columns; letter++) {
-                    switch(line[letter]){
-                        case 82: gameObjects.push_back(GameObject(2, Position({letter, dimension})));
-                            break;
-                        case 114: gameObjects.push_back(GameObject(3, Position({letter, dimension})));
-                            break;
-                        case 72: gameObjects.push_back(GameObject(0, Position({letter, dimension})));
-                            break;
-                        case 104: gameObjects.push_back(GameObject(1, Position({letter, dimension})));
-                            break;
-                        case 42: gameObjects.push_back(GameObject(4, Position({letter, dimension})));
-                            break;
-                        case 43: gameObjects.push_back(GameObject(5, Position({letter, dimension})));
-                            break;
-                        case 79: gameObjects.push_back(GameObject(6, Position({letter, dimension})));
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                dimension++;
+                //  3 19 754 984
+                int last_space = 0;
+                std::string volume = line.substr(last_space, line.find(' '));
+                last_space = last_space + 1 + line.find(' ');
+                std::string weight = line.substr(last_space, line.substr(last_space).find(' '));
+                last_space = last_space + 1 + line.substr(last_space).find(' ');
+                std::string reward = line.substr(last_space, line.substr(last_space).find(' '));
+                last_space = last_space + 1 + line.substr(last_space).find(' ');
+                std::string duration = line.substr(last_space, line.substr(last_space).find(' '));
+                std::cout << line << std::endl;
+                std::cout << "volume:" << volume << " weight:" << weight << " reward:" << reward << " duration:" << duration << std::endl;
+                deliveries->emplace_back(std::stoi(volume), std::stoi(weight), std::stoi(reward), std::stoi(duration)); //Comment this to test it!
             }
         }
     }
-    maze.setMaze(n_lines, n_columns);
+    else {
+        std::cout << "Failed to open" << std::endl;
+    }
     // Close the MyReadFile
     MyReadFile.close();
-    return gameObjects;
-};
- */
+    return;
+}
+
+void Helper::ReadVehicles(std::vector<Vehicle> *vehicles) {
+    std::ifstream MyReadFile("../Input/carrinhas.txt");
+    int first_line = 0;
+    if (MyReadFile.is_open()) {
+        std::string line;
+        while (getline(MyReadFile, line)) {
+            if (first_line == 0) {
+                first_line++;
+            }
+            else {
+                //  3 19 754 984
+                int last_space = 0;
+                std::string volume = line.substr(last_space, line.find(' '));
+                last_space = last_space + 1 + line.find(' ');
+                std::string weight = line.substr(last_space, line.substr(last_space).find(' '));
+                last_space = last_space + 1 + line.substr(last_space).find(' ');
+                std::string cost = line.substr(last_space, line.substr(last_space).find(' '));
+                std::cout << line << std::endl;
+                std::cout << "volume:" << volume << " weight:" << weight << " cost:" << cost << std::endl;
+                vehicles->emplace_back(std::stoi(volume), std::stoi(weight), std::stoi(cost));
+            }
+        }
+    }
+    else {
+        std::cout << "Failed to open" << std::endl;
+    }
+    // Close the MyReadFile
+    MyReadFile.close();
+    return;
+}
--- Classes/Helper.h
+++ Classes/Helper.h
@@ -5,9 +5,25 @@
 #ifndef PROJETODA1_HELPER_H
 #define PROJETODA1_HELPER_H
 
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "Vehicle.h"
+#include "Delivery.h" //Comment this to test it
+
+//class Delivery; Uncomment this to test
 
 class Helper {
-    void ReadDeliveries();
+public:
+    /**
+     * Reads the values from the file encomendas.txt creating objects that will be stored in the @param deliveries vector.
+     */
+    void ReadDeliveries(std::vector<Delivery> *deliveries);
+    /**
+     * Reads the values from the file carrinhas.txt creating objects that will be stored in the @param vehicles vector.
+     */
+    void ReadVehicles(std::vector<Vehicle> *vehicles);
 };
 
 
