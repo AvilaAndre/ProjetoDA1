5dfa46d1592cf9d53c460f83d3db5128626066a7
André Ávila <avila_andre14@hotmail.com>
2022-04-15 20:29:33 +0100

    Delivery.h documentation.

--- Classes/Delivery.h
+++ Classes/Delivery.h
@@ -20,11 +20,34 @@ private:
     int reward;
     int duration;
 public:
+    /**
+     * The constructor of the delivery class.
+     * @param express Defines if it is an express delivery or a normal delivery.
+     * @param wei Defines the value of weight.
+     * @param vol Defines the value of volume.
+     * @param rew Defines the value of reward.
+     * @param dur Defines the value of duration.
+     */
     Delivery(bool express, int wei, int vol, int rew, int dur);
+    /**
+     * @return Returns a string with a few information about this class, primarily created for debugging.
+     */
     std::string toString();
+    /**
+     * @return Returns the weight value.
+     */
     int getWeight();
+    /**
+     * @return Returns the volume value.
+     */
     int getVolume();
+    /**
+     * @return Returns the reward value.
+     */
     int getReward();
+    /**
+     * @return Returns the duration value.
+     */
     int getDuration();
 };
 
